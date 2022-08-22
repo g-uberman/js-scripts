87570393222550f63446dc104ddf833fed02a785
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-08-22 18:30:59 +0200

    budget-items.js added

---
+++ budget-items.js
@@ -0,0 +1,75 @@
+// VALUES
+
+bank = prompt( "Budget" );
+bank = Number( bank );
+
+iprice = prompt ( "Item price (net)" );
+iprice = Number ( iprice );
+
+tax_rate = prompt ( "Tax (%)" );
+tax_rate = Number ( tax_rate ) / 100;
+
+var numOfItems = 0;
+var amount = iprice;
+
+// FUNCTIONS
+
+function calcAmountTaxed() {
+	amount = amount + ( amount * tax_rate );
+	}
+
+function calciPriceTaxed() {
+	iprice = iprice + ( iprice * tax_rate );
+	}
+
+function fixed2All () {
+	bank = bank.toFixed( 2 );
+	iprice = iprice.toFixed( 2 );
+	amount = amount.toFixed( 2 );
+}
+
+function printTotal() {
+	total = numOfItems * iprice;
+	totalPrint = "Total price: $" + total.toFixed( 2 );
+	console.log( totalPrint);
+	}
+	
+function printBank() {
+	bankPrint = "Budget: $" + bank.toFixed( 2 );
+	console.log ( bankPrint );
+	}
+
+function printiPrice() {
+	ipricePrint = "Item price: $" + iprice.toFixed( 2 );
+	console.log ( ipricePrint);
+	}
+
+function printItems() {
+	itemPrint = "Number of items bought: " + numOfItems;
+	console.log ( itemPrint);
+	}
+
+// RUNS
+
+calcAmountTaxed();
+calciPriceTaxed();
+
+while (true) {
+if ( amount <= bank) {
+	console.log( "Item bought" );
+	numOfItems = numOfItems +1;
+	amount = amount + iprice;
+	}
+else {
+	break;
+	}
+}
+
+// fixed2All();
+
+// PRINTS
+
+printBank();
+printiPrice();
+printItems();
+printTotal();
